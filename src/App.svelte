e1ff6912917ea3f82264513f608c0fbf7b66a5e1
Pablo Tissera <pablo.tissera@silstech.com.ar>
2021-04-18 13:21:50 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { each } from "svelte/internal";
+import IssueCard from "./IssueCard.svelte";
+
+  const columns = [
+    {name: "backlog", cards: []},
+    {name: "Started", cards: []},
+    {name: "In Progress", cards: []},
+    {name: "Blocked", cards: []},
+    {name: "Code Review", cards: [{name: 'Problema con los estilos', description: ' problema para ver los border', status: 'code review', author: 'p Tissera', created: '2021-04-12', updated: '2021-04-12', comments: [], severity: 3}]},
+    {name: "Testing", cards: []},
+    {name: "Canceled", cards: []},
+    {name: "Closed", cards: []},
+  ];
+</script>
+
+<main>
+  <h1>Kanban</h1>
+
+  <div class="main-container">
+    {#each columns as {name, cards}}
+      <div class="column card bg-primary shadow-soft border-light">
+        <span class="column-header">{name}</span>
+        <div class="column-body">
+			{#each cards as card}
+		      <IssueCard {card}/>
+			{/each}
+		</div>
+      </div>
+    {/each}
+  </div>
+</main>
+<style>
+
+  .column-header {
+	  font-weight: bold;
+	  border-bottom: 1px solid var(--gray-500);
+  }
+
+  .column-body {
+	height: calc(100vh - 10rem);
+	padding: .5em;
+	
+  }
+
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+    gap: 1em;
+    flex-grow: 1;
+    width: 100%;
+  }
+  .column {
+    width: 100%;
+  }
+  main {
+    text-align: center;
+  }
+
+  h1 {
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 150;
+  }
+</style>
---
+++ src/IssueCard.svelte
@@ -0,0 +1,67 @@
+<script>
+    export let card;
+</script>
+<div class="issue-card shadow-soft border-light">
+    <div class="issue-card-header border-light">{card.name}<span class="issue-severity">{card.severity}</span></div>
+    <div class="issue-card-body">
+        <b>Description: </b>
+        {card.description}
+    </div>
+    <div class="issue-card-footer border-light" title="Created"> {card.created} <author title="author">{card.author}</author></div>
+</div>
+<style>
+  .issue-card {
+	border-radius: .6em;
+  }
+
+  legend {
+      font-size: .8rem;
+      font-weight: bold;
+  }
+
+  .issue-card-header {
+	padding: .3em 0;
+	border-bottom: 1px solid;
+    display: block;
+    overflow: hidden;
+    background-color: var(--gray-400);
+    border-radius: .7em .7em 0 0;
+  }
+
+  .issue-severity {
+    float: right;
+    border: 1px solid var(--gray-500);
+    background-color: var(--gray-500);
+    top: -0px;
+    right: -2px;
+    padding: .1em .5em;
+    border-radius: .8em 0 0 .8em;
+    position: relative;
+  }
+  .issue-card-footer {
+    border-top: 1px solid;
+    font-size: .8rem;
+    padding: .5em;
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--gray-400);
+    border-radius: 0 0 .7em .7em;
+  }
+
+  .issue-card-body {
+      font-size: .8rem;
+      padding: .2em .8em;
+      width: 100%;
+      max-height: 10em;
+      overflow-x: hidden;
+      overflow-y: auto;
+  }
+
+  author {
+      background-color: var(--gray-200);
+      border-radius: 1em;
+      padding: 0 .5em;
+  }
+
+
+</style>
